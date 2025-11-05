f1a57b427cbdd9f10c293e1dbfffc240319ff1eb
Rehan Raju <20068389@mydbs.ie.>
2025-11-06 04:09:02 +0530

    made change

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,14 +46,12 @@ resource "aws_internet_gateway" "gw" {
   count  = var.vpc_id == "" ? 1 : 0
   vpc_id = aws_vpc.main[0].id
 }
-
 resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = var.vpc_id != "" ? data.aws_vpc.selected[0].id : aws_vpc.main[0].id
-  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
-  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  count = 2
+  vpc_id = var.vpc_id
+  cidr_block = element(["10.0.0.0/24", "10.0.1.0/24"], count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
-
   tags = {
     Name = "public-subnet-${count.index}"
   }
